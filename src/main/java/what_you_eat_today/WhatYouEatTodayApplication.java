525e71e5ce165f7fc31083a7fc7d906d73b28c18
GodManV2 <manthesplit508@gmail.com>
2025-05-24 23:47:59 +0700

    initial

---
+++ src/main/java/what_you_eat_today/WhatYouEatTodayApplication.java
@@ -0,0 +1,13 @@
+package what_you_eat_today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatYouEatTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatYouEatTodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/what_you_eat_today/WhatYouEatTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package what_you_eat_today;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatYouEatTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
